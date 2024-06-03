5a57426d73cecac926d25f5298cbcc022ecd29d3
szr <406260188@qq.com>
2024-06-03 08:54:26 +0800

    apitest后端接口

---
+++ users/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+from rest_framework import status
+from rest_framework.request import Request
+from rest_framework.response import Response
+from rest_framework_simplejwt.exceptions import TokenError, InvalidToken
+
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+
+# Create your views here.
+
+
+class LoginView(TokenObtainPairView):
+    def post(self, request: Request, *args, **kwargs) -> Response:
+        serializer = self.get_serializer(data=request.data)
+
+        try:
+            serializer.is_valid(raise_exception=True)
+        except TokenError as e:
+            raise InvalidToken(e.args[0])
+
+        result = serializer.validated_data
+        res = {}
+        res['access_token'] = result['access']
+        res['refresh_token'] = result['refresh']
+
+        return Response(res, status=status.HTTP_200_OK)
